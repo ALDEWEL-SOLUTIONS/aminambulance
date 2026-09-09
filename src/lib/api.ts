35dccfe3ffa6e4d9d27313a00ce9aeda946665f5
Qozeem <kezman42006@gmail.com>
2026-09-09 14:32:52 +0100

    Add Vercel API proxy

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
   //baseURL: 'http://my911webservice.runasp.net/api/v1',
-  baseURL: import.meta.env.VITE_API_URL,
-  //baseURL: '/api/v1',
+  //baseURL: import.meta.env.VITE_API_URL,
+  baseURL: '/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
